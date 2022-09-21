bdcf252aed7495504443b92c7715f482fb6f2243
may-Jay <marybassey7@gmail.com>
2022-09-21 22:41:43 +0100

    Task 14

---
+++ IdeaProjects/FirstJavaProject/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("Just wrote my first Java Project.");
+        System.out.println("A big shout out to Testify Academy for the Switch to Software Testing course");
+        System.out.println("This is project is for Task 14");
+    }
+}
